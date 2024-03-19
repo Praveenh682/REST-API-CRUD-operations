87e370c7818de69c44f723f75e07f7243df4e9a7
Krishvanth <krishvanth@Krishvanths-MacBook-Air.local>
2024-03-19 22:16:25 +0530

    first commit

---
+++ dj-REST/core/home/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Person
+
+class PeopleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Person
+        fields=['name','age']
+
+
--- dj-REST/core/home/views.py
+++ dj-REST/core/home/views.py
@@ -1,3 +1,44 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from home.models import Person
+from home.serializers import PeopleSerializer
+
+
+@api_view(['GET','POST'])
+
+def index(request):
+    if request.method == 'GET':
+        json_response={
+            "name":"Praveen",
+            "courses":"Python",
+            'method':'GET'
+        }
+    else :
+        data = request.data
+        print(data)
+        json_response={
+            "name":"Praveen",
+            "courses":"Python",
+            'method':'POST'
+        }
+    return Response(json_response)
+
+@api_view(['GET','POST'])
+def person(request):
+    if request.method == 'GET':
+        objs = Person.objects.all()
+        serializer = PeopleSerializer(objs, many =True)
+        return Response(serializer.data)
+    
+    else:
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    
+
 
